f296091636776d19fe97728604602a23e41aa1db
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-04-29 18:17:41 +0100

    chore: update project configuration and add linting support
    - Added ESLint and Prettier for code quality and formatting.
    - Created .eslintrc configuration file extending @react-native-community rules.
    - Updated package.json to include lint script and new dependencies.
    - Added babel.config.js for Babel configuration.

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View
-} from "react-native";
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { SearchBar } from "react-native-paper";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>Search</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <SearchBar placeholder="Search" />
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+        <View style={{ padding: 2 }}>
           <Text>List</Text>
         </View>
       </SafeAreaView>
@@ -23,4 +18,8 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1, marginTop: StatusBar.currentHeight },
+  search: { padding: 16, backgroundColor: "green" },
+  text: { flex: 1, padding: 16, backgroundColor: "blue" },
+});
