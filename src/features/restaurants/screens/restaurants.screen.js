26d5881edc5f5eca425c76f701f2aabf2e3728f4
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-04-30 15:48:17 +0100

    Set up theming and update the styling

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { SafeAreaView, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
-import styled from "styled-components";
+import styled from "styled-components/native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
@@ -10,12 +10,11 @@ const SafeArea = styled(SafeAreaView)`
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
 `;
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 const RestaurantListContainer = styled.View`
   flex: 1;
-  padding: 16px;
-  background-color: blue;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => {
---
+++ src/infrastructure/theme/index.js
@@ -0,0 +1,14 @@
+import { colors } from "./colors";
+import { sizes } from "./sizes";
+import { lineHeight, space } from "./spacing";
+import { fonts, fontSizes, fontWeights } from "./fonts";
+
+export const theme = {
+  colors,
+  space,
+  lineHeight,
+  sizes,
+  fonts,
+  fontWeights,
+  fontSizes,
+};
