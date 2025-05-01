8472affbc463db970c1041d575c01b1982024184
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-05-01 11:25:48 +0100

    add the address

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Card } from "react-native-paper";
 import { Text } from "react-native";
+import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
@@ -11,8 +18,8 @@ const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  padding: ${(props) => props.theme.space[3]};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
 
@@ -32,7 +39,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
