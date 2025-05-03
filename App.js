66bf10ef6c7f638ffa1a044f5f738b10fc9b51df
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-05-03 19:41:54 +0100

    feat: add bottom tab navigation
    - Integrated `createBottomTabNavigator` for navigation between screens.
    - Added `RestaurantsScreen`, `MapScreen`, and `SettingsScreen` as tabs.

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
+import { View, Text } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import {
   useFonts as useOswald,
@@ -9,6 +12,24 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+
+const SettingsScreen = () => {
+  return (
+    <SafeArea>
+      <Text>Settings!</Text>
+    </SafeArea>
+  );
+};
+const MapScreen = () => {
+  return (
+    <SafeArea>
+      <Text>Map!</Text>
+    </SafeArea>
+  );
+};
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -25,7 +46,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
