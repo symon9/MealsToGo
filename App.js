cb1c79951e3d0dbf250e680122d1f741bb21504d
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-05-05 19:15:09 +0100

    feat: configure bottom tab navigation with custom screen options
    - Added `createScreenOptions` function to dynamically set tab icons.
    - Configured `screenOptions` in `Tab.Navigator` for active and inactive tint colors.
    - hide the header

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import {
   useFonts as useOswald,
@@ -13,6 +14,12 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
+
+const TAB_ICON = {
+  Restaurants: "restaurant",
+  Map: "map",
+  Settings: "settings",
+};
 
 const SettingsScreen = () => {
   return (
@@ -31,6 +38,16 @@ const MapScreen = () => {
 
 const Tab = createBottomTabNavigator();
 
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -47,7 +64,17 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              ...createScreenOptions({ route }),
+              tabBarActiveTintColor: "tomato",
+              tabBarInactiveTintColor: "gray",
+              headerShown: false,
+              tabBarStyle: {
+                height: 60,
+              },
+            })}
+          >
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={MapScreen} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
