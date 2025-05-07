6d0c1456fde515531fba7ecefb3e0e93f73b5d78
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-05-07 20:28:54 +0100

    add camelize dependency and update restaurants transformation logic

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,5 @@
 import { mocks } from "./mock";
+import camelize from "camelize";
 
 export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
@@ -10,10 +11,13 @@ export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
   });
 };
 
-restaurantRequest()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log("Error! - ", err);
+const restaurantsTransform = (results = []) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
   });
+  return camelize(mappedResults);
+};
