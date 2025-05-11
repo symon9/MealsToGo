f479c29e3f1c638a6eadf5d71c2ca58a20c44e2e
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-05-11 20:12:48 +0100

    feat: Add mock data and services for restaurants and locations
    - Implemented mock data for restaurants in Antwerp, Chicago, Toronto, and San Francisco.
    - Created `restaurants.service.js` to handle restaurant data requests and transformations.
    - Added `location.service.js` to handle location-based mock data retrieval and transformation.
    - Updated `restaurant-info-card.component.js` to display restaurant details, including ratings, status, and address.
    - Integrated mock images for restaurant photos.

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,9 +1,16 @@
-import React, { useState, useEffect, useMemo, createContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  createContext,
+  useContext,
+} from "react";
 
 import {
   restaurantsRequest,
   restaurantsTransform,
 } from "./restaurants.service";
+import { LocationContext } from "../location/location.context";
 
 export const RestaurantsContext = createContext();
 
@@ -11,11 +18,13 @@ export const RestaurantsContextProvider = ({ children }) => {
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const { location } = useContext(LocationContext);
 
-  const retrieveRestaurants = () => {
+  const retrieveRestaurants = (loc) => {
     setIsLoading(true);
+    setRestaurants([]);
     setTimeout(() => {
-      restaurantsRequest()
+      restaurantsRequest(loc)
         .then(restaurantsTransform)
         .then((result) => {
           setIsLoading(false);
@@ -29,8 +38,11 @@ export const RestaurantsContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    retrieveRestaurants();
-  }, []);
+    if (location) {
+      const locationString = `${location.lat},${location.lng}`;
+      retrieveRestaurants(locationString);
+    }
+  }, [location]);
 
   return (
     <RestaurantsContext.Provider value={{ restaurants, isLoading, error }}>
