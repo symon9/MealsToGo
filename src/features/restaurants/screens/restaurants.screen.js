244bfecc4a990021906785111f418e00d6500792
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-05-11 02:03:36 +0100

    fix outdated color lib

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { FlatList } from "react-native";
-import { ActivityIndicator, Searchbar } from "react-native-paper";
+import { ActivityIndicator, Searchbar, MD2Colors } from "react-native-paper";
 import styled from "styled-components/native";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
@@ -32,7 +32,7 @@ export const RestaurantsScreen = () => {
     <SafeArea>
       {isLoading && (
         <LoadingContainer>
-          <Loading animating={true} size={50} />
+          <Loading animating={true} size={50} color={MD2Colors.blue300} />
         </LoadingContainer>
       )}
       <SearchContainer>
