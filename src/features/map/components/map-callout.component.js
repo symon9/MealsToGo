8c19123dced84fdafe0c790079d323cc2e8322b5
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-05-13 20:22:08 +0100

    feat: add map callout

---
+++ src/features/map/components/map-callout.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components/native";
+
+import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
+
+export const MapCallout = ({ restaurant }) => {
+  return <CompactRestaurantInfo restaurant={restaurant} />;
+};
--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,17 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Callout, Marker } from "react-native-maps";
 import styled from "styled-components/native";
 
 import { Search } from "../components/search.component";
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { MapCallout } from "../components/map-callout.component";
 
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
 
@@ -46,7 +47,17 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              <Callout
+                onPress={() =>
+                  navigation.navigate("RestaurantDetail", {
+                    restaurant,
+                  })
+                }
+              >
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
           );
         })}
       </Map>
