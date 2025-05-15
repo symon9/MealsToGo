40e88059950c89204ab82a9ee18a99f597e46166
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-05-15 13:03:53 +0100

    Add AsyncStorage for favourites management and update CompactRestaurantInfo component

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -23,12 +23,12 @@ const Item = styled.View`
 
 const isAndroid = Platform.OS === "android";
 
-export const CompactRestaurantInfo = ({ restaurant }) => {
-  const Image = isAndroid ? CompactWebView : CompactImage;
+export const CompactRestaurantInfo = ({ restaurant, isMap }) => {
+  const Image = isAndroid && isMap ? CompactWebView : CompactImage;
 
   return (
     <Item>
-      <CompactImage source={{ uri: restaurant.photos[0] }} />
+      <Image source={{ uri: restaurant.photos[0] }} />
       <Text center variant="caption" numberOfLines={3}>
         {restaurant.name}
       </Text>
