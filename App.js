1096f1ff352ac2986ce1f599dfafe7c20b505bff
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-05-15 19:12:49 +0100

    Install firebase

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import { initializeApp } from 'firebase/app';
 
 import {
   useFonts as useOswald,
@@ -8,12 +9,22 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
+import { Navigation } from "./src/infrastructure/navigation";
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
-import { Navigation } from "./src/infrastructure/navigation";
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "mealstogo-001.firebaseapp.com",
+  projectId: "mealstogo-001",
+  storageBucket: "mealstogo-001.firebasestorage.app",
+  messagingSenderId: "",
+  appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
